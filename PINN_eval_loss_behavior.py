b711d6787d4a14d8c8cdcc6d087e39320ab72c8a
dzj2734@fels.scc.kit.edu <hgf_dzj2734@haicn1992.localdomain>
2025-02-28 12:55:27 +0100

    eval functions updated

--- PINN_eval_loss_behavior.py
+++ PINN_eval_loss_behavior.py
@@ -12,6 +12,8 @@ from typing import Any
 from flax import struct
 from flax.serialization import to_state_dict, from_state_dict
 import pickle
+from scipy.io import loadmat
+from scipy.interpolate import interpn
 from pathlib import Path
 import matplotlib.pyplot as plt
 import scipy.stats as st
@@ -60,7 +62,6 @@ class PINN(PINNbase):
         valid_data = self.c.problem.exact_solution(all_params)
         model_fn = c.network.network_fn
         return all_params, model_fn, train_data, valid_data
-
 def equ_func(all_params, g_batch, cotangent, model_fns):
     def u_t(batch):
         return model_fns(all_params, batch)
@@ -120,7 +121,7 @@ if __name__ == "__main__":
     from PINN_problem import *
     import argparse
     from glob import glob
-    #checkpoint_fol = "TBL_run_test36"
+    #checkpoint_fol = "TBL_run_test61"
     parser = argparse.ArgumentParser(description='TBL_PINN')
     parser.add_argument('-c', '--checkpoint', type=str, help='checkpoint', default="")
     args = parser.parse_args()
@@ -145,66 +146,102 @@ if __name__ == "__main__":
     run = PINN(c)
     #checkpoint_list = np.sort(glob(run.c.model_out_dir+'/*.pkl'))
     #with open(run.c.model_out_dir + "saved_dic_720000.pkl","rb") as f:
-    
-    checkpoint_list = sorted(glob(run.c.model_out_dir+'/*.pkl'), key=lambda x: int(x.split('_')[4].split('.')[0]))
-    print(checkpoint_list)
-    with open(checkpoint_list[-1],'rb') as f:
-        a = pickle.load(f)
     all_params, model_fn, train_data, valid_data = run.test()
+    
 
-    model = Model(all_params["network"]["layers"], model_fn)
-    all_params["network"]["layers"] = from_state_dict(model, a).params
-    dynamic_params = all_params["network"]["layers"]
-    indexes, counts = np.unique(train_data['pos'][:,0], return_counts=True)
+
 #%%
     u_tau = 15*10**(-6)/36.2/10**(-6)
     u_ref_n = 4.9968*10**(-2)/u_tau
     delta = 36.2*10**(-6)
     x_ref_n = 1.0006*10**(-3)/delta
 
-#%% temporal error는 51개의 시간단계에대해서 [:,0]는 velocity error, [:,1]은 pressure error
-    output_shape = (213,141,61)
-    temporal_error_vel_list = []
-    temporal_error_pre_list = []
-    temporal_error_acc_list = []
-    for j in range(51):
-        print(j)
-        acc = np.concatenate([acc_cal(all_params["network"]["layers"], all_params, train_data['pos'][np.sum(counts[:j]):np.sum(counts[:(j+1)])][10000*s:10000*(s+1)], model_fn) 
-                              for s in range(train_data['pos'][np.sum(counts[:j]):np.sum(counts[:(j+1)])].shape[0]//10000+1)],0)
-        pred = model_fn(all_params, valid_data['pos'].reshape((51,)+output_shape+(4,))[j,:,:,:,:].reshape(-1,4))
-        output_keys = ['u', 'v', 'w', 'p']
-        output_unnorm = [all_params["data"]['u_ref'],all_params["data"]['v_ref'],
-                        all_params["data"]['w_ref'],1.185*all_params["data"]['u_ref']]
-        outputs = {output_keys[i]:pred[:,i]*output_unnorm[i] for i in range(len(output_keys))}
-        outputs['p'] = outputs['p'] - np.mean(outputs['p'])
-        output_ext = {output_keys[i]:valid_data['vel'].reshape((51,)+output_shape+(4,))[j,:,:,:,i].reshape(-1) for i in range(len(output_keys))}
-        output_ext['p'] = output_ext['p'] - 0.0025*all_params['domain']['in_max'][0,1]*valid_data['pos'].reshape((51,)+output_shape+(4,))[0,:,:,:,1].reshape(-1)*1.185*x_ref_n/u_ref_n**2
-        output_ext['p'] = output_ext['p'] - np.mean(output_ext['p'])
+    timestep = 24
+
+    datapath = '/home/hgf_dlr/hgf_dzj2734/TBL/PG_TBL_dnsinterp.mat'
+    data = loadmat(datapath)
+    eval_key = ['x', 'y', 'z', 'x_pred', 'y_pred', 'z_pred', 'u1', 'v1', 'w1', 'p1', 'um', 'vm', 'wm']
+    DNS_grid = (0.001*data['y'][:,0,0], 0.001*data['x'][0,:,0], 0.001*data['z'][0,0,:])
+    eval_grid = np.concatenate([0.001*data['y_pred'].reshape(32,88,410)[:31,:,:].reshape(-1,1),
+                                0.001*data['x_pred'].reshape(32,88,410)[:31,:,:].reshape(-1,1),
+                                0.001*data['z_pred'].reshape(32,88,410)[:31,:,:].reshape(-1,1)],1)
+    vel_ground = [interpn(DNS_grid, data[eval_key[i+6]], eval_grid).reshape(31,88,410) for i in range(3)]
+    fluc_ground = [interpn(DNS_grid, data[eval_key[i+6]], eval_grid).reshape(31,88,410) - data[eval_key[i+10]].reshape(32,88,410)[:31,:,:] for i in range(3)]
+    p_cent = interpn(DNS_grid, data['p1'], eval_grid).reshape(31,88,410)
+    fluc_ground.append(p_cent-np.mean(p_cent))
+    V_mag = np.sqrt(fluc_ground[0]**2+fluc_ground[1]**2+fluc_ground[2]**2)
+    div_list = [V_mag, V_mag, V_mag, fluc_ground[-1]]
+    fluc_ground[-1] = (fluc_ground[-1]*u_ref_n**2 - 0.0025*eval_grid[:,1].reshape(31,88,410)[0,0,:]*x_ref_n)/u_ref_n**2
 
 
-        f = np.concatenate([(outputs['u']-output_ext['u']).reshape(-1,1), 
-                            (outputs['v']-output_ext['v']).reshape(-1,1), 
-                            (outputs['w']-output_ext['w']).reshape(-1,1)],1)
-        div = np.concatenate([output_ext['u'].reshape(-1,1), output_ext['v'].reshape(-1,1), 
-                            output_ext['w'].reshape(-1,1)],1)
-        f2 = np.concatenate([(acc[:,0]-train_data['acc'][np.sum(counts[:j]):np.sum(counts[:(j+1)])][:,0]).reshape(-1,1), 
-                             (acc[:,1]-train_data['acc'][np.sum(counts[:j]):np.sum(counts[:(j+1)])][:,1]).reshape(-1,1), 
-                             (acc[:,2]-train_data['acc'][np.sum(counts[:j]):np.sum(counts[:(j+1)])][:,2]).reshape(-1,1)],1)
+    checkpoint_list = sorted(glob(run.c.model_out_dir+'/*.pkl'), key=lambda x: int(x.split('_')[4].split('.')[0]))
+    pre_error_list = []
+    vel_error_list = []
+    acc_error_list = []
+    pos_ref = all_params["domain"]["in_max"].flatten()
+    vel_ref = np.array([all_params["data"]["u_ref"],
+                        all_params["data"]["v_ref"],
+                        all_params["data"]["w_ref"]])
+    ref_key = ['t_ref', 'x_ref', 'y_ref', 'z_ref', 'u_ref', 'v_ref', 'w_ref']
+    ref_data = {ref_key[i]:ref_val for i, ref_val in enumerate(np.concatenate([pos_ref,vel_ref]))}
+
+    eval_grid_n = np.concatenate([np.zeros((eval_grid.shape[0],1))+timestep*(1/17594),
+                                eval_grid[:,1:2], eval_grid[:,0:1], eval_grid[:,2:3]],1)
+    for i in range(eval_grid_n.shape[1]): eval_grid_n[:,i] = eval_grid_n[:,i]/ref_data[ref_key[i]] 
+
+    p_cent3 = p_cent - 0.0025*eval_grid[:,1].reshape(31,88,410)[0,0,:]*1.185*x_ref_n/u_ref_n**2
+    p_cent3 = p_cent3 - np.mean(p_cent3)    
+    indexes, counts = np.unique(train_data['pos'][:,0], return_counts=True)
+    g = 0
+
+    for checkpoint in checkpoint_list:
+        print(g)
+        with open(checkpoint,'rb') as f:
+            a = pickle.load(f)
+
+        model = Model(all_params["network"]["layers"], model_fn)
+        all_params["network"]["layers"] = from_state_dict(model, a).params
+
+
+        acc = np.concatenate([acc_cal(all_params["network"]["layers"], 
+                                      all_params, 
+                                      train_data['pos'][np.sum(counts[:timestep]):np.sum(counts[:(timestep+1)])][10000*s:10000*(s+1)], 
+                                      model_fn) 
+                              for s in range(train_data['pos'][np.sum(counts[:timestep]):np.sum(counts[:(timestep+1)])].shape[0]//10000+1)],0)
+        pred = np.concatenate([model_fn(all_params, 
+                                        eval_grid_n[10000*i:10000*(i+1),:]) 
+                               for i in range(eval_grid_n.shape[0]//10000+1)],0)
+        ref_key = ['t_ref', 'x_ref', 'y_ref', 'z_ref', 'u_ref', 'v_ref', 'w_ref', 'u_ref']
+        pred_ = np.concatenate([pred[:,i:i+1]*ref_data[ref_key[i+4]] for i in range(4)],1)
+        pred_[:,-1] = pred_[:,-1]*1.185
+        pred_[:,-1] = pred_[:,-1] - np.mean(pred_[:,-1])
+        fluc_pred = [pred_[:,i].reshape(31,88,410) - data[eval_key[i+10]].reshape(32,88,410)[:31,:,:] for i in range(3)]
+
+        f = np.concatenate([(fluc_pred[0]-fluc_ground[0]).reshape(-1,1), 
+                            (fluc_pred[1]-fluc_ground[1]).reshape(-1,1), 
+                            (fluc_pred[2]-fluc_ground[2]).reshape(-1,1)],1)
+        div = np.concatenate([fluc_ground[0].reshape(-1,1), fluc_ground[1].reshape(-1,1), 
+                            fluc_ground[2].reshape(-1,1)],1)
+        f2 = np.concatenate([(acc[:,0]-train_data['acc'][np.sum(counts[:timestep]):np.sum(counts[:(timestep+1)])][:,0]).reshape(-1,1), 
+                                (acc[:,1]-train_data['acc'][np.sum(counts[:timestep]):np.sum(counts[:(timestep+1)])][:,1]).reshape(-1,1), 
+                                (acc[:,2]-train_data['acc'][np.sum(counts[:timestep]):np.sum(counts[:(timestep+1)])][:,2]).reshape(-1,1)],1)
         div2 = np.concatenate([train_data['acc'][:,0].reshape(-1,1), train_data['acc'][:,1].reshape(-1,1), 
-                               train_data['acc'][:,2].reshape(-1,1)],1)  
-        temporal_error_pre_list.append(np.linalg.norm(outputs['p'] - output_ext['p'])/np.linalg.norm(output_ext['p']))
-        temporal_error_vel_list.append(np.linalg.norm(f, ord='fro')/np.linalg.norm(div,ord='fro'))    
-        temporal_error_acc_list.append(np.linalg.norm(f2, ord='fro')/np.linalg.norm(div2,ord='fro'))
-    temporal_error = np.concatenate([np.array(temporal_error_vel_list).reshape(-1,1),
-                                     np.array(temporal_error_pre_list).reshape(-1,1),
-                                     np.array(temporal_error_acc_list).reshape(-1,1)],1)
+                                train_data['acc'][:,2].reshape(-1,1)],1)   
+        pre_error_list.append(np.linalg.norm(pred_[:,-1:].reshape(31,88,410)-p_cent3)/np.linalg.norm(p_cent3))
+        vel_error_list.append(np.linalg.norm(f, ord='fro')/np.linalg.norm(div,ord='fro'))
+        acc_error_list.append(np.linalg.norm(f2, ord='fro')/np.linalg.norm(div2,ord='fro'))
+        g = g+1
+    pre_error = np.array(pre_error_list)
+    vel_error = np.array(vel_error_list)
+    acc_error = np.array(acc_error_list)
+    tol_error = np.concatenate([vel_error.reshape(-1,1),pre_error.reshape(-1,1),acc_error.reshape(-1,1)],1)
 
-#%%
     if os.path.isdir("datas/"+checkpoint_fol):
         pass
     else:
         os.mkdir("datas/"+checkpoint_fol)
 
-    with open("datas/"+checkpoint_fol+"/temporal_error.pkl","wb") as f:
-        pickle.dump(temporal_error,f)
+    with open("datas/"+checkpoint_fol+"/error_evolution.pkl","wb") as f:
+        pickle.dump(tol_error,f)
     f.close()
+
